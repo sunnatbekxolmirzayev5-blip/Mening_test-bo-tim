b120a074c68b23bdf18b52af0f4835e892910d2a
sunnatbekxolmirzayev5-blip <sunnatbekxolmirzayev5@gmail.com>
2026-02-21 01:00:10 +0500

    Update API token and welcome message

--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
+
 import telebot
 
-# Botingiz tokini
-API_TOKEN = '8234527643:AAG3ldC-MD4dYyZUfoOnUHcqACZDjeo2c94'
+# BotFather'dan olgan tokeningizni mana shu qo'shtirnoq ichiga yopishtiring
+API_TOKEN = '8041216411:AAHfLKlXQ6ltm4Gtn2MAiwMTw-nBp71hmbg'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Salom! Men GitHub-ga yuklangan botman.")
+    bot.reply_to(message, "Salom! Bot ishga tushdi.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
 bot.infinity_polling()
+
