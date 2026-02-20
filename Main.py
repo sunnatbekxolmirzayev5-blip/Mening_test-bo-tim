0da00eb518dabc223078c1d869a973294b71dadb
sunnatbekxolmirzayev5-blip <sunnatbekxolmirzayev5@gmail.com>
2026-02-21 00:50:37 +0500

    Enhance bot with echo feature and use infinity polling
    Added echo functionality and changed polling method.

--- Main.py
+++ Main.py
@@ -1,12 +1,16 @@
 import telebot
 
-# BotFather'dan olgan tokeningizni ' ' belgilari orasiga qo'ying
+# Botingiz tokini
 API_TOKEN = '8234527643:AAG3ldC-MD4dYyZUfoOnUHcqACZDjeo2c94'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Salom! Men GitHub-ga yuklangan botman.")
+    bot.reply_to(message, "Salom! Men GitHub-ga yuklangan botman.")
 
-bot.polling()
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
